b275183a4eebd591de1c34e4847e8138f2bf4e36
sketchdinee317 <120928122+sketchdinee317@users.noreply.github.com>
2022-12-18 17:38:24 +0530

    Add files via upload

---
+++ mirror reverse.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 18 17:34:28 2022
+
+@author: admin
+"""
+
+x=("edyoda")
+reverse=''
+i=len(x)-1
+while i>=0:
+    reverse=reverse+x[i]
+    i=i-1
+print(reverse)
